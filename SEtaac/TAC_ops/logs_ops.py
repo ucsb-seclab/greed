1f963f0c628d0e6d4283c71c57264273509cfaf7
Nicola Ruaro <ruaronicola@gmail.com>
2022-06-16 15:12:13 -0700

    use decorators in log ops

--- SEtaac/TAC_ops/logs_ops.py
+++ SEtaac/TAC_ops/logs_ops.py
@@ -1,6 +1,6 @@
-from .base import TAC_Statement
+from SEtaac.TAC_ops.base import TAC_Statement
                   
-from ..state import SymbolicEVMState
+from SEtaac.state import SymbolicEVMState
 
 __all__ = [
     'TAC_Log0','TAC_Log1','TAC_Log2','TAC_Log3','TAC_Log4'
@@ -21,14 +21,14 @@ b. Logs are kept track of during tx execution exactly the same way as selfdestru
 c. The ordered list of logs in the transaction are expressed as [log0, log1, ..., logN].
 """
 
-
-class TAC_Log0(TAC_Statement):
-    __internal_name__ = "LOG0"
+class TAC_Log(TAC_Statement):
+    __internal_name__ = "LOG"
     __aliases__ = {
                    'offset_var': 'arg1_var', 'offset_val': 'arg1_val',
                    'size_var'  : 'arg2_var', 'size_val'  : 'arg2_val'
                   }
 
+    @TAC_Statement.handler_with_side_effects
     def handle(self, state:SymbolicEVMState):
         succ = state.copy()
         arg1 = succ.registers[self.arg1_var]
@@ -43,6 +43,13 @@ class TAC_Log0(TAC_Statement):
         succ.set_next_pc()
         return [succ]
 
+class TAC_Log0(TAC_Statement):
+    __internal_name__ = "LOG0"
+    __aliases__ = {
+                   'offset_var': 'arg1_var', 'offset_val': 'arg1_val',
+                   'size_var'  : 'arg2_var', 'size_val'  : 'arg2_val'
+                  }
+
 class TAC_Log1(TAC_Statement):
     __internal_name__ = "LOG1"
     __aliases__ = {
@@ -50,21 +57,6 @@ class TAC_Log1(TAC_Statement):
                    'size_var'    : 'arg2_var'  , 'size_val'    : 'arg2_val',
                    'topic_var'   : 'arg3_var'  , 'topic_val'   : 'arg3_val'
                    }
-
-    def handle(self, state:SymbolicEVMState):
-        succ = state.copy()
-        arg1 = succ.registers[self.arg1_var]
-        arg2 = succ.registers[self.arg2_var]
-        arg3 = succ.registers[self.arg3_var]
-
-        mstart, msz = arg1, arg2
-        succ.memory.extend(mstart, msz)
-        # Ignore external effects...
-        # depth = 1
-        # topics = [arg3]
-
-        succ.set_next_pc()
-        return [succ]
 
 class TAC_Log2(TAC_Statement):
     __internal_name__ = "LOG2"
@@ -75,21 +67,6 @@ class TAC_Log2(TAC_Statement):
                    'topic2_var'  : 'arg4_var'  , 'topic2_val'  : 'arg4_val'
                    }
 
-    def handle(self, state:SymbolicEVMState):
-        succ = state.copy()
-        arg1 = succ.registers[self.arg1_var]
-        arg2 = succ.registers[self.arg2_var]
-        arg3 = succ.registers[self.arg3_var]
-        arg4 = succ.registers[self.arg4_var]
-
-        mstart, msz = arg1, arg2
-        succ.memory.extend(mstart, msz)
-        # Ignore external effects...
-        # depth = 2
-        # topics = [arg3, arg4]
-
-        succ.set_next_pc()
-        return [succ]
 
 class TAC_Log3(TAC_Statement):
     __internal_name__ = "LOG3"
@@ -101,23 +78,6 @@ class TAC_Log3(TAC_Statement):
                    'topic3_var'  : 'arg5_var'  , 'topic3_val'  : 'arg5_val'
                    }
 
-    def handle(self, state:SymbolicEVMState):
-        succ = state.copy()
-        arg1 = succ.registers[self.arg1_var]
-        arg2 = succ.registers[self.arg2_var]
-        arg3 = succ.registers[self.arg3_var]
-        arg4 = succ.registers[self.arg4_var]
-        arg5 = succ.registers[self.arg5_var]
-
-        mstart, msz = arg1, arg2
-        succ.memory.extend(mstart, msz)
-        # Ignore external effects...
-        # depth = 3
-        # topics = [arg3, arg4, arg5]
-
-        succ.set_next_pc()
-        return [succ]
-
 class TAC_Log4(TAC_Statement):
     __internal_name__ = "LOG4"
     __aliases__ = {
@@ -128,21 +88,3 @@ class TAC_Log4(TAC_Statement):
                    'topic3_var'  : 'arg5_var'  , 'topic3_val'  : 'arg5_val',
                    'topic4_var'  : 'arg6_var'  , 'topic4_val'  : 'arg6_val'
                    }
-
-    def handle(self, state:SymbolicEVMState):
-        succ = state.copy()
-        arg1 = succ.registers[self.arg1_var]
-        arg2 = succ.registers[self.arg2_var]
-        arg3 = succ.registers[self.arg3_var]
-        arg4 = succ.registers[self.arg4_var]
-        arg5 = succ.registers[self.arg5_var]
-        arg6 = succ.registers[self.arg6_var]
-
-        mstart, msz = arg1, arg2
-        succ.memory.extend(mstart, msz)
-        # Ignore external effects...
-        # depth = 4
-        # topics = [arg3, arg4, arg5, arg6]
-
-        succ.set_next_pc()
-        return [succ]
