c1470a8af56221b3314de6af701d713d90fca899
Nicola Ruaro <ruaronicola@gmail.com>
2022-06-15 21:48:26 -0700

    update test code

--- SEtaac/tests/test_base.py
+++ SEtaac/tests/test_base.py
@@ -1,25 +1,15 @@
 from SEtaac import Project
+from SEtaac.utils import gen_exec_id
 
 p = Project("./IR_DICT.dill",
             "./TAC_CFG.dill")
 
-project = p 
+xid = gen_exec_id()
+entry_state = p.factory.entry_state(xid=1)
+simgr = p.factory.simgr(entry_state=entry_state)
 
-block = p.factory.block('0x0')
-state = p.factory.entry_state(1)
+simgr.run()
 
-simgr = p.factory.simgr(entry_state=state)
-simgr.step()
-
-# for x in blocks:
-#     for s in x:
-#         if s.__internal_name__ == 'CALLPRIVATE':
-#             if s.res_vars:
-#                 pass
 
 import IPython; IPython.embed()
 # import ipdb; ipdb.set_trace()
-
-#xid = gen_exec_id()
-#entry_state = p.factory.entry_state(xid=1)
-#simulation_manager = p.factory.simgr(entry_state=entry_state)
