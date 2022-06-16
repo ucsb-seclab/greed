9562ae5c08f758cb37d9a160306fbc0f40a79342
Nicola Ruaro <ruaronicola@gmail.com>
2022-06-15 22:22:09 -0700

    fix SHA3

--- SEtaac/TAC_ops/special_ops.py
+++ SEtaac/TAC_ops/special_ops.py
@@ -30,30 +30,30 @@ class TAC_Sha3(TAC_Binary):
         arg1 = succ.registers[self.op1_var]
         arg2 = succ.registers[self.op2_var]
 
-        state.memory.extend(arg1, arg2)
-        mm = state.memory.read(arg1, arg2)
+        succ.memory.extend(arg1, arg2)
+        mm = succ.memory.read(arg1, arg2)
         if not isinstance(mm, SymRead) and all(concrete(m) for m in mm):
             data = utils.bytearray_to_bytestr(mm)
             succ.registers[self.res_var] = utils.big_endian_to_int(utils.sha3(data))
         else:
             if not isinstance(mm, SymRead):
                 sha_data = z3.simplify(z3.Concat([m if z3.is_expr(m) else z3.BitVecVal(m, 8) for m in mm]))
-                for k, v in state.sha_constraints.items():
+                for k, v in succ.sha_constraints.items():
                     if isinstance(v, SymRead):
                         continue
                     if v.size() == sha_data.size() and is_true(v == sha_data):
                         sha = k
                         break
                 else:
-                    sha = z3.BitVec('SHA3_%x_%d' % (state.instruction_count, state.xid), 256)
-                    state.sha_constraints[sha] = sha_data
+                    sha = z3.BitVec('SHA3_%x_%d' % (succ.instruction_count, succ.xid), 256)
+                    succ.sha_constraints[sha] = sha_data
             else:
                 sha_data = mm
-                sha = z3.BitVec('SHA3_%x_%d' % (state.instruction_count, state.xid), 256)
-                state.sha_constraints[sha] = sha_data
+                sha = z3.BitVec('SHA3_%x_%d' % (succ.instruction_count, succ.xid), 256)
+                succ.sha_constraints[sha] = sha_data
             succ.registers[self.res_var] = sha
 
-        succ.pc = succ.set_next_pc()
+        succ.set_next_pc()
         return [succ]
 
 class TAC_Stop(TAC_NoOperandsNoRes):
