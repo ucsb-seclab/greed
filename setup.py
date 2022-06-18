56b0e345b8b11f701fed74aac3dc9de2e177d0c0
Nicola Ruaro <ruaronicola@gmail.com>
2022-06-17 17:31:12 -0700

    fix setup.py requirements

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ setup(
     install_requires=[
         'z3-solver>=4.8.17.0',
         'pysha3>=1.0.2',
+        'matplotlib>=3.5.2',
         'networkx>=2.5.1',
         'web3>=5.29.2',
         'dill>=0.3.4',
         'solc-select>=0.2.1',
-        'teether@git+ssh://git@github.com/ucsb-seclab/teether_shellphish.git'
+        'teether@git+ssh://git@github.com/nescio007/teether.git'
     ],
     python_requires='>=3.5',
 )
