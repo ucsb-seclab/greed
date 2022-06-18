42264184d52989d7fc5e5598fc948699172e0df7
Nicola Ruaro <ruaronicola@gmail.com>
2022-06-18 16:21:38 -0700

    separate analysis of .hex vs .hex.raw

--- scripts/analyze_hex_raw.sh
+++ scripts/analyze_hex_raw.sh
@@ -4,7 +4,7 @@ if [[ $# = 0 ]]; then
   echo usage: $0 \<contract .hex file\>
   exit 1
 elif [ -f $1 ]; then
-  SOURCE_FILE=$1
+  HEX_RAW_FILE=$1
 else
   echo $1 is not a file
   exit 1
@@ -12,9 +12,9 @@ fi
 
 SETAAC_DIR=`dirname "${BASH_SOURCE[0]}"`
 SETAAC_DIR=`readlink -f $SETAAC_DIR/../`
+GIGAHORSE_DIR=$SETAAC_DIR/gigahorse-toolchain
 
-# compile with solc-select
-SOLC_VERSION=0.8.7 solc --bin source.sol | sed "1,/Binary:/d" > contract.hex.raw
+$SETAAC_DIR/scripts/extract_contract_code.py $HEX_RAW_FILE > contract.hex
 
-# analyze hex raw
-$SETAAC_DIR/scripts/analyze_hex_raw.sh contract.hex.raw
+# analyze hex
+$SETAAC_DIR/scripts/analyze_hex.sh contract.hex
