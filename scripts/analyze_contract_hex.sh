2a31a57e53089b963eea42bce626653f2f63ab8e
Nicola Ruaro <ruaronicola@gmail.com>
2022-06-21 14:42:54 -0700

    dl_compiled scripts are actually gigahorse clients

--- scripts/analyze_contract_hex.sh
+++ scripts/analyze_contract_hex.sh
@@ -18,7 +18,7 @@ GIGAHORSE_DIR=$SETAAC_DIR/gigahorse-toolchain
 arch=$(uname -i)
 
 $GIGAHORSE_DIR/generatefacts $HEX_FILE facts &&
-LD_LIBRARY_PATH=$SETAAC_DIR/scripts/lib/$arch/ $SETAAC_DIR/scripts/main.$arch.dl_compiled -F facts &&
+LD_LIBRARY_PATH=$GIGAHORSE_DIR/clients/lib/$arch/ $GIGAHORSE_DIR/clients/main.$arch.dl_compiled -F facts &&
 $GIGAHORSE_DIR/clients/visualizeout.py &&
 $GIGAHORSE_DIR/clients/check_bad_opcode.py &&
 $GIGAHORSE_DIR/clients/export_ir.py &&
