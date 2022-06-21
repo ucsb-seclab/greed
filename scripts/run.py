f2a4e058d823c06d5e60525cdff376c3f3ce5b98
Nicola Ruaro <ruaronicola@gmail.com>
2022-06-21 13:59:20 -0700

    allow to force concrete calldata and function identifier

--- scripts/run.py
+++ scripts/run.py
@@ -2,6 +2,7 @@
 import IPython
 import argparse
 import logging
+import z3
 
 from SEtaac import Project
 from SEtaac.utils import gen_exec_id
@@ -16,6 +17,24 @@ def main(args):
 
     xid = gen_exec_id()
     entry_state = p.factory.entry_state(xid=xid)
+
+    # use concrete calldata if possible
+    if args.calldata is not None:
+        log.info(f"Setting CALLDATA to a concrete value: {args.calldata}")
+        calldata_int = int(args.calldata, 16)
+        calldata_bytes = calldata_int.to_bytes(length=(calldata_int.bit_length() + 7) // 8, byteorder='big')
+        entry_state.calldata = [z3.BitVecVal(b, 256) for b in calldata_bytes]
+        entry_state.calldatasize = z3.BitVecVal(len(calldata_bytes), 256)
+        if len(calldata_bytes) > 256:
+            log.error("CALLDATA longer than the current max length (256 bytes)")
+            exit(1)
+    elif args.function_id is not None:
+        log.info(f"Adding constraints for function id: {args.function_id}")
+        function_id_int = int(args.function_id, 16)
+        function_id_bytes = function_id_int.to_bytes(length=(function_id_int.bit_length() + 7) // 8, byteorder='big')
+        for b in enumerate(function_id_bytes):
+            entry_state.constraints.append(entry_state.calldata[i] == b)
+
     simgr = p.factory.simgr(entry_state=entry_state)
 
     try:
@@ -31,6 +50,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("target", type=str, action="store", help="Path to Gigahorse output folder")
+    parser.add_argument("--calldata", type=str, action="store", help="Concrete CALLDATA (in hex)")
+    parser.add_argument("--function-id", type=str, action="store", help="Concrete function identifier (in hex)")
     parser.add_argument("-d", "--debug", action="store_true", help="Enable debug output")
 
     args = parser.parse_args()
