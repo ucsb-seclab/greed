f3fa2d545d17b466f36ff419b3407282037721ad
Nicola Ruaro <ruaronicola@gmail.com>
2022-06-22 09:52:37 -0700

    fix registers copy

--- SEtaac/registers.py
+++ SEtaac/registers.py
@@ -9,3 +9,9 @@ class SymbolicRegisters(dict):
         if concrete(value):
             value %= 2 ** 256
         super(SymbolicRegisters, self).__setitem__(key, value)
+
+    def copy(self):
+        cls = self.__class__
+        new = cls.__new__(cls)
+        new.update(self)
+        return new
