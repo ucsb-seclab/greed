fb69b394bd93378ffdc89ed10f3fe1db1bee07ae
Nicola Ruaro <ruaronicola@gmail.com>
2022-06-25 11:39:48 -0700

    clean setup.sh

--- setup.sh
+++ setup.sh
@@ -10,20 +10,19 @@ cd $SETAAC_DIR
 git submodule update --init --recursive
 
 # link our clients into gigahorse-toolkit
-for client in $SETAAC_DIR/gigahorse_clients/*; do
+for client in $SETAAC_DIR/gigahorse_clients/{*.dl_compiled,*.py,lib} $SETAAC_DIR/gigahorse_clients/lib; do
   ln -sf $client $SETAAC_DIR/gigahorse-toolchain/clients/
 done
 
 # link our scripts into virtualenv's bin dir
-for script in $SETAAC_DIR/scripts/*; do
+for script in $SETAAC_DIR/scripts/{*.sh,*.py}; do
   ln -sf $script $VIRTUAL_ENV/bin/
 done
 
 # create alias for run.py
 ln -sf $SETAAC_DIR/scripts/run.py $VIRTUAL_ENV/bin/SEtaac
 
-# this is needed by solc-select
-sudo apt install mkisofs
-solc-select install 0.8.7
+command -v >&- mkisofs || echo "mkisofs is not installed. solc-select might not work correctly (sudo apt install mkisofs)"
+solc-select versions | grep -q 0.8.7 || solc-select install 0.8.7
 
-# we probably don't need to setup gigahorse-toolkit if we use the pre-compiled datalog
+# NOTE: no need to setup gigahorse-toolkit since we use the pre-compiled datalog
