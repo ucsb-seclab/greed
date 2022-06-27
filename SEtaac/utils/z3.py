1d3f4798946fd950e9fc76c706a1e9a071905ca3
Nicola Ruaro <ruaronicola@gmail.com>
2022-06-26 23:45:43 -0700

    add some z3 utils

--- SEtaac/utils/z3.py
+++ SEtaac/utils/z3.py
@@ -126,3 +126,94 @@ def translate_xid(expr, old_xid, new_xid):
                 raise Exception('CANNOT CONVERT %s (%d)' % (v, v.sort_kind()))
     subst = list(substitutions.items())
     return z3.substitute(expr, subst)
+
+
+def get_all_models(solver, terminals, n_max=256):
+    if len(terminals) == 0:
+        assert solver.check() == z3.sat
+        return [solver.model()]
+
+    t = terminals[0]
+
+    # get all the solutions for the current terminal
+    solutions = eval_n_terminal(solver, t, n_max)
+
+    # for every solution: backup the solver, fix the solution, recurse, restore the solver
+    models = []
+    for solution in solutions:
+        # log.debug(f'processing solution {solution} for terminal {t}')
+        solver.push()
+        solver.add(t == solution)
+        models += get_all_models(solver, terminals[1:], n_max)
+        solver.pop()
+
+    return models
+
+
+def get_all_constraints(solver, expression, model):
+    if isinstance(expression, (AbstractString, AbstractChar)):
+        expression = expression.bv_repr
+
+    terminals = get_vars_non_recursive(solver, expression)
+    return [t == model[t] for t in terminals]
+
+
+def eval_n_terminal(solver, t, n):
+    solver.push()
+    solutions = []
+    for _ in range(n):
+        if solver.check() == z3.unsat:
+            break
+        model = solver.model()
+        sol = model.eval(t, model_completion=True)
+        solutions += [sol]
+
+        # enforce distinct solutions
+        solver.add(t != sol)
+    solver.pop()
+
+    return solutions
+
+
+def eval_all_expression(solver, expression, verifier=lambda expression: None, n_max=256):
+    if isinstance(expression, (AbstractString, AbstractChar)):
+        expression = expression.bv_repr
+
+    terminals = get_vars_non_recursive(expression)
+    models = get_all_models(solver, terminals, n_max)
+    solutions = {model: model.eval(expression, model_completion=True).as_string() for model in models}
+
+    for model, solution in dict(solutions).items():
+        try:
+            verifier(solution)
+        except:
+            del solutions[model]
+
+    return solutions
+
+
+def eval_one_expression(solver, expression, verifier=lambda expression: None):
+    solutions = eval_all_expression(solver, expression, verifier, n_max=1)
+    return dict([list(solutions.items())[0], ])
+
+
+def get_one_model(solver):
+    terminals = []
+
+    for expression in solver.assertions():
+        terminals += get_vars_non_recursive(expression)
+
+    return get_all_models(solver, terminals, 1)[0]
+
+
+def get_all_terminals(solver):
+    terminals = []
+
+    for expression in solver.assertions():
+        terminals += get_vars_non_recursive(expression, include_select=True)
+
+    return terminals
+
+
+def eval_one_array(model, array, length):
+    return [model.eval(array[i], model_completion=True).as_long() for i in range(length)]
