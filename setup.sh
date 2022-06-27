ca3e2378186334ccc3eb48ff7f4caee6ddd0ad53
Nicola Ruaro <ruaronicola@gmail.com>
2022-06-27 14:40:32 -0700

    consider gigahorse dependencies

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+bold=$(tput bold)
+normal=$(tput sgr0)
+red=$(tput setaf 160)
+
 j=1
 
 while (( $# >= 1 )); do
@@ -23,13 +27,14 @@ echo "Initializing gigahorse submodule.."
 git submodule update --init --recursive
 
 # compile gigahorse clients
-command -v >&- souffle || { echo "souffle is not installed. Please install it before proceeding (https://souffle-lang.github.io/build)"; exit 1; }
+command -v >&- souffle || { echo "${bold}${red}souffle is not installed. Please install it before proceeding (https://souffle-lang.github.io/build, version 2.0.2 preferred)${normal}"; exit 1; }
+dpkg -l | grep libboost-all-dev || echo "${bold}${red}libboost-all-dev is not installed. Please install it before proceeding (sudo apt install libboost-all-dev)${normal}"
 
 echo "Compiling gigahorse clients. This will take some time.."
 echo "Number of parallel datalog jobs: $j (override with $0 -j N)"
 
 cd $GIGAHORSE_DIR/souffle-addon
-make &> /dev/null || { echo "Failed to build gigahorse's souffle-addon"; exit 1; }
+make &> /dev/null || { echo "${bold}${red}Failed to build gigahorse's souffle-addon${normal}"; exit 1; }
 cd $SETAAC_DIR
 souffle --jobs 1 -M "GIGAHORSE_DIR=$GIGAHORSE_DIR BULK_ANALYSIS=" -o $GIGAHORSE_DIR/clients/main.dl_compiled $GIGAHORSE_DIR/logic/main.dl -L $GIGAHORSE_DIR/souffle-addon
 
@@ -49,5 +54,5 @@ done
 echo "Creating alias run.py -> SEtaac.."
 ln -sf $SETAAC_DIR/scripts/run.py $VIRTUAL_ENV/bin/SEtaac
 
-command -v >&- mkisofs || echo "mkisofs is not installed. solc-select might not work correctly (sudo apt install mkisofs)"
+command -v >&- mkisofs || echo "${bold}${red}mkisofs is not installed. solc-select might not work correctly (sudo apt install mkisofs)${normal}"
 solc-select versions | grep -q 0.8.7 || { echo "Installing solc 0.8.7"; solc-select install 0.8.7; }
