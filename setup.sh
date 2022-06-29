8169d5120d252b0b13a80c13301ef3325b2204a7
Nicola Ruaro <ruaronicola@gmail.com>
2022-06-28 20:00:03 -0700

    fix --no-gigahorse flag

--- setup.sh
+++ setup.sh
@@ -38,12 +38,12 @@ done
 echo "Creating alias run.py -> SEtaac.."
 ln -sf $SETAAC_DIR/scripts/run.py $VIRTUAL_ENV/bin/SEtaac
 
+echo $NO_GIGAHORSE
+
 if [ -z $NO_GIGAHORSE ]; then
-  true;
-else
   # compile gigahorse clients
-  command -v >&- souffle || { echo "${bold}${red}souffle is not installed. Please install it before proceeding (https://souffle-lang.github.io/build, version 2.0.2 preferred)${normal}"; exit 1; }
-  dpkg -l | grep -q libboost-all-dev || { echo "${bold}${red}libboost-all-dev is not installed. Please install it before proceeding (e.g., sudo apt install libboost-all-dev)${normal}"; exit 1; }
+  command -v >&- souffle || { echo "${bold}${red}souffle is not installed. Please install it before proceeding (https://souffle-lang.github.io/build, version 2.0.2 preferred)${normal}"; echo "${bold}${red}Or maybe you forgot --no-gigahorse?${normal}"; exit 1; }
+  dpkg -l | grep -q libboost-all-dev || { echo "${bold}${red}libboost-all-dev is not installed. Please install it before proceeding (e.g., sudo apt install libboost-all-dev)${normal}"; echo "${bold}${red}Or maybe you forgot --no-gigahorse?${normal}"; exit 1; }
 
   echo "Compiling gigahorse clients. This will take some time.."
   echo "Number of parallel datalog jobs: $j (override with $0 -j N)"
@@ -63,4 +63,6 @@ else
 
   command -v >&- mkisofs || echo "${bold}${red}mkisofs is not installed. solc-select might not work correctly (e.g., sudo apt install mkisofs)${normal}"
   solc-select versions | grep -q 0.8.7 || { echo "Installing solc 0.8.7"; solc-select install 0.8.7; }
+else
+  true
 fi
