bc53539094b05789688602936278d95fc6987041
degrigis <degrigis@ucsb.edu>
2022-07-28 00:00:18 -0700

    more comments
    Signed-off-by: degrigis <degrigis@ucsb.edu>

--- SEtaac/exploration_techniques/__init__.py
+++ SEtaac/exploration_techniques/__init__.py
@@ -2,18 +2,31 @@
 
 class ExplorationTechnique:
 
-    # Any operation that
-    def setup():
-        raise Exception
+    # Any operations that needs to be done
+    # on the simulation manager before starting 
+    # the exploration with this technique.
+    def setup(self, simgr):
+        return
 
-    def check_stashes():
-        raise Exception
+    # This method receives the current active 
+    # stashes that can be manipulated/re-ordered
+    # etc... 
+    # This MUST return the stashes.
+    def check_stashes(self, stashes):
+        return stashes
 
-    def check_stashes():
-        raise Exception
+    # This method receives the state 
+    # we are going to generate the 
+    # successors for.
+    # This MUST return the state.
+    def check_state(self, state):
+        return state
 
-    def check_stashes():
-        raise Exception
+    # This method receives all the successors
+    # generated from a step of a state.
+    # This MUST return the successors.
+    def check_successors(self, successors):
+        return successors
 
 
 from .simgrviz import SimgrViz
