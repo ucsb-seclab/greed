bac18ee68d0ddc082ec55f76112c0a9b765b89c9
Nicola Ruaro <ruaronicola@gmail.com>
2022-07-29 19:22:56 -0700

    update playground forward exploration

--- scripts/playground_forward_exploration.py
+++ scripts/playground_forward_exploration.py
@@ -8,64 +8,11 @@ from SEtaac import Project
 from SEtaac import options
 from SEtaac.utils import gen_exec_id
 from SEtaac.utils.solver.shortcuts import *
+from SEtaac.exploration_techniques import DFS, DirectedSearch
 
 LOGGING_FORMAT = "%(levelname)s | %(name)s | %(message)s"
 logging.basicConfig(level=logging.INFO, format=LOGGING_FORMAT)
 log = logging.getLogger("SEtaac")
-
-
-def is_reachable_without_returns(block_a, block_b, factory, callgraph):
-    function_a = block_a.function
-    function_b = block_b.function
-    if block_a == block_b:
-        return True
-    elif function_a == function_b:
-        # check if reachable in intra-procedural cfg
-        return block_b in block_a.descendants
-    elif function_a and function_b:
-        # for each path (in the callgraph) from function_a to function_b
-        callgraph_paths = nx.all_simple_paths(callgraph, function_a, function_b)
-        for path in callgraph_paths:
-            first_call_target = path[1]
-            # check if we can reach the first call (i.e., any "CALLPRIVATE <first_call_target>" in function_a)
-            for callprivate_block_id in function_a.callprivate_target_sources[first_call_target.id]:
-                callprivate_block = factory.block(callprivate_block_id)
-                return is_reachable_without_returns(block_a, callprivate_block, factory, callgraph)
-    else:
-        return False
-
-
-def is_reachable(state_a, block_b):
-    factory = state_a.project.factory
-    callgraph = state_a.project.callgraph
-
-    block_a = factory.block(state_a.curr_stmt.block_id)
-    if is_reachable_without_returns(block_a, block_b, factory, callgraph):
-        # this is the simple case, no need to look at the callstack
-        return True
-    elif not state_a.callstack:
-        # not reachable without returns, but the callstack is empty
-        return False
-    else:
-        # otherwise we can look at the callstack
-        callstack = list(state_a.callstack)
-        while callstack:
-            return_stmt_id, _ = callstack.pop()
-            return_stmt = factory.statement(return_stmt_id)
-            return_block = factory.block(return_stmt.block_id)
-
-            # check if any RETURNPRIVATE is reachable
-            for returnprivate_block_id in block_a.function.returnprivate_block_ids:
-                returnprivate_block = factory.block(returnprivate_block_id)
-                if is_reachable_without_returns(block_a, returnprivate_block, factory, callgraph):
-                    break
-            else:
-                # executed if there is no break
-                return False
-
-            if is_reachable_without_returns(return_block, block_b, factory, callgraph):
-                return True
-        return False
 
 
 def find_paths_with_stmt(p, target_stmt):
@@ -140,12 +87,33 @@ def main(args):
         print('Block not found.')
         exit(1)
 
-    # todo: consider all critical paths
-    critical_paths = find_paths_with_stmt(p, target_stmt)
-    if not critical_paths:
-        log.fatal('No paths found')
-        exit()
-    critical_path = critical_paths[0]
+    xid = gen_exec_id()
+    entry_state = p.factory.entry_state(xid=xid)
+
+    simgr = p.factory.simgr(entry_state=entry_state)
+
+    options.LAZY_SOLVES = True
+
+    directed_search = DirectedSearch(target_stmt)
+    simgr.use_technique(directed_search)
+
+    dfs = DFS()
+    simgr.use_technique(dfs)
+
+    try:
+        simgr.run()
+    except KeyboardInterrupt:
+        pass
+
+    import IPython; IPython.embed(); exit()
+
+
+    # # todo: consider all critical paths
+    # critical_paths = find_paths_with_stmt(p, target_stmt)
+    # if not critical_paths:
+    #     log.fatal('No paths found')
+    #     exit()
+    # critical_path = critical_paths[0]
 
     # this is to hi-jack a call
     # found.curr_stmt.set_arg_val(found)
