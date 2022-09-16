73f597ccd5ae2d4e38cee53da40b96f65d044776
degrigis <degrigis@ucsb.edu>
2022-09-16 16:31:57 +0000

    re-pushed decompile.sh
    Signed-off-by: degrigis <degrigis@ucsb.edu>

---
+++ scripts/decompile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FILEPATH=`readlink -f "${BASH_SOURCE[0]}"`
+SETAAC_DIR=`dirname $FILEPATH`
+SETAAC_DIR=`readlink -f $SETAAC_DIR/../`
+GIGAHORSE_DIR=$SETAAC_DIR/gigahorse-toolchain
+
+arch=$(uname -i)
+
+# decompile
+if [ -f $GIGAHORSE_DIR/clients/source_decompiler.$arch.dl_compiled ]; then
+  LD_LIBRARY_PATH=$GIGAHORSE_DIR/souffle-addon/ $GIGAHORSE_DIR/clients/source_decompiler.$arch.dl_compiled &&
+  $GIGAHORSE_DIR/clients/get_source.py
+else
+  echo Decompilation not supported on arch $arch
+  exit 1
+fi
