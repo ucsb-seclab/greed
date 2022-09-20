74f1538573d2655b2373cee9275487352cf893d1
degrigis <degrigis@ucsb.edu>
2022-09-20 03:39:19 +0000

    fixed constraints on sha
    Signed-off-by: degrigis <degrigis@ucsb.edu>

--- SEtaac/sha3.py
+++ SEtaac/sha3.py
@@ -61,7 +61,7 @@ class Sha3(LambdaMemory):
 
         bounded_ackermann_constraint = If(And(*([sha_data_len_is_equal] + bounded_bytes_are_equal)),
                                           sha_equal,
-                                          And(sha_not_equal))
+                                          sha_not_equal)
 
         self.add_constraint(bounded_ackermann_constraint)
     
