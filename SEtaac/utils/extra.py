07e7f38f7438b3e0022dcd20b3d01374b36ccfc3
degrigis <degrigis@ucsb.edu>
2022-10-24 19:09:04 +0000

    added utilities for exp calculation
    Signed-off-by: degrigis <degrigis@ucsb.edu>

--- SEtaac/utils/extra.py
+++ SEtaac/utils/extra.py
@@ -12,3 +12,17 @@ XID_GENERATOR = UUIDGenerator()
 
 def gen_exec_id():
     return XID_GENERATOR.next()
+
+
+def is_pow2(x):
+    return x and not x & (x - 1)
+
+
+def log2(x):
+    if not is_pow2(x):
+        raise ValueError("%d is not a power of 2!" % x)
+    i = -1
+    while x:
+        x >>= 1
+        i += 1
+    return i
