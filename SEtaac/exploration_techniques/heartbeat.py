14944dbb97017246e0bee092dce2d47f8558282c
degrigis <degrigis@ucsb.edu>
2023-01-05 20:08:10 +0000

    added option to print ops in Heartbeat
    Signed-off-by: degrigis <degrigis@ucsb.edu>

--- SEtaac/exploration_techniques/heartbeat.py
+++ SEtaac/exploration_techniques/heartbeat.py
@@ -13,13 +13,14 @@ class HeartBeat(ExplorationTechnique):
     The heartbeat file will be logged during __init__.
     Delete such file to stop the heartbeat and get an ipdb shell.
     """
-    def __init__(self, beat_interval=100):
+    def __init__(self, beat_interval=100, show_op=False):
         super(HeartBeat, self).__init__()
         self.heart_beat_file = tempfile.NamedTemporaryFile(prefix='SEtaac_heartbeat_', delete=False).name
         log.info(f"Heartbeat file: {self.heart_beat_file} (delete to stop heartbeat)")
         self.beat_interval = beat_interval
         self.beat_cnt = 0
         self.steps_cnt = 0
+        self.show_op = show_op
  
     def check_successors(self, simgr, successors):
         self.beat_cnt += 1
@@ -27,6 +28,8 @@ class HeartBeat(ExplorationTechnique):
         if self.beat_cnt == self.beat_interval:
             log.info("Exploration is alive <3. Step {}".format(self.steps_cnt))
             log.info(f"Simgr: {simgr} (active: {simgr.active})")
+            if self.show_op:
+                log.info(f"State: {simgr.active[0]} (curr_stmt: {simgr.active[0].curr_stmt})")
             self.beat_cnt = 0
             if not os.path.isfile(self.heart_beat_file):
                 log.info("HeartBeat stopped, need help? </3")
