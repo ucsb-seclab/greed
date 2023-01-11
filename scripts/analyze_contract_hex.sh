9fa88c82d1f47f298e9afa86d4988fcb05ad5981
Nicola Ruaro <ruaronicola@gmail.com>
2023-01-10 19:59:02 -0800

    rewrite analyze_contract_hex

--- scripts/analyze_contract_hex.sh
+++ scripts/analyze_contract_hex.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-INLINING_ROUNDS=6
-CONTEXT_DEPTH=8
-
 while (( $# >= 1 )); do
     case $1 in
     --file) HEX_FILE=$2; shift; shift;;
-    --context-depth) CONTEXT_DEPTH=$2; shift; shift;;
-    --inlining-rounds) INLINING_ROUNDS=$2; shift; shift;;
     *) break;
     esac;
 done
 
 if [[ -z $HEX_FILE ]]; then
-  echo usage: analyze_contract_hex.sh --file \<contract .hex file\> [--inlining-rounds \<num inlining rounds\>]
+  echo usage: analyze_contract_hex.sh --file \<contract .hex file\>
   exit 1
 elif [ ! -f $HEX_FILE ]; then
   echo $HEX_FILE is not a file
@@ -25,36 +20,18 @@ SETAAC_DIR=`dirname $FILEPATH`
 SETAAC_DIR=`readlink -f $SETAAC_DIR/../`
 GIGAHORSE_DIR=$SETAAC_DIR/gigahorse-toolchain
 
-arch=$(uname -i)
-
 if [ ! -f $GIGAHORSE_DIR/clients/main.dl_compiled ]; then
   echo "Can't find main.dl_compiled (something went wrong in setup.sh)"
-  exit 1
-elif [ ! -f $GIGAHORSE_DIR/clients/function_inliner.dl_compiled ]; then
-  echo "Can't find function_inliner.dl_compiled (something went wrong in setup.sh)"
   exit 1
 elif [ ! -f $GIGAHORSE_DIR/clients/guards.dl_compiled ]; then
   echo "Can't find guards.dl_compiled (something went wrong in setup.sh)"
   exit 1
 fi
 
-echo "Running main.dl"
-$GIGAHORSE_DIR/generatefacts $HEX_FILE facts &&
-echo "Using context depth: $CONTEXT_DEPTH (override with --context-depth)" &&
-echo $CONTEXT_DEPTH > facts/MaxContextDepth.csv &&
-LD_LIBRARY_PATH=$GIGAHORSE_DIR/souffle-addon/ $GIGAHORSE_DIR/clients/main.dl_compiled -F facts || { echo "${bold}${red}Failed to run main.dl_compiled${normal}"; exit 1; }
-
-echo "Running $INLINING_ROUNDS rounds of inlining (override with --inlining-rounds)"
-for i in $(seq 1 $INLINING_ROUNDS); do
-  echo "Running inlining round $i.."
-  LD_LIBRARY_PATH=$GIGAHORSE_DIR/souffle-addon/ $GIGAHORSE_DIR/clients/function_inliner.dl_compiled || { echo "${bold}${red}Failed to run function_inliner.dl_compiled${normal}"; exit 1; }
-done
-
-echo "Running guards.dl"
-LD_LIBRARY_PATH=$GIGAHORSE_DIR/souffle-addon/ $GIGAHORSE_DIR/clients/guards.dl_compiled || { echo "${bold}${red}Failed to run guards.dl_compiled${normal}"; exit 1; }
-
-echo "Running visualizeout.py (to compute .tac output)"
-$GIGAHORSE_DIR/clients/visualizeout.py || { echo "${bold}${red}Failed to run visualizeout.py${normal}"; exit 1; }
-
-echo "Checking bad opcodes"
-$GIGAHORSE_DIR/clients/check_bad_opcode.py || { echo "${bold}${red}Failed to run check_bad_opcode.py${normal}"; exit 1; }
+echo "Running gigahorse.py"
+/usr/bin/time -v $GIGAHORSE_DIR/gigahorse.py -q QUIET -T 3600 --disable_inline -C $GIGAHORSE_DIR/clients/visualizeout.py,$GIGAHORSE_DIR/clients/guards.dl_compiled $HEX_FILE &> exec_info &&
+curr_dir=$(pwd); cd $GIGAHORSE_DIR; gigahorse_version=$(git rev-parse HEAD); cd $curr_dir; printf "\tGigahorse version: $gigahorse_version\n" >> exec_info &&
+curr_dir=$(pwd); cd $SETAAC_DIR; greed_version=$(git rev-parse HEAD); cd $curr_dir; printf "\tgreed version: $greed_version\n" >> exec_info &&
+cp .temp/contract/out/* . &&
+cp .temp/contract/contract.dasm .temp/contract/solidity_version.csv . &&
+rm -rf .temp bytecode.hex
