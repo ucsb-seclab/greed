ba18a19c6d27dc7a77e8ac62e0b32b910f7f73ea
Nicola Ruaro <ruaronicola@gmail.com>
2023-01-16 12:30:47 -0800

    configurable timeout

--- scripts/analyze_contract_hex.sh
+++ scripts/analyze_contract_hex.sh
@@ -3,12 +3,16 @@
 while (( $# >= 1 )); do
     case $1 in
     --file) HEX_FILE=$2; shift; shift;;
+    --timeout) TIMEOUT=$2; shift; shift;;
     *) break;
     esac;
 done
 
 if [[ -z $HEX_FILE ]]; then
-  echo usage: analyze_contract_hex.sh --file \<contract .hex file\>
+  echo usage: analyze_contract_hex.sh --file \<contract .hex file\> --timeout \<timeout\>
+  exit 1
+elif [[ -z $TIMEOUT ]]; then
+  echo usage: analyze_contract_hex.sh --file \<contract .hex file\> --timeout \<timeout\>
   exit 1
 elif [ ! -f $HEX_FILE ]; then
   echo $HEX_FILE is not a file
@@ -29,7 +33,7 @@ elif [ ! -f $GIGAHORSE_DIR/clients/guards.dl_compiled ]; then
 fi
 
 echo "Running gigahorse.py"
-/usr/bin/time -v $GIGAHORSE_DIR/gigahorse.py -q QUIET -T 180 --reuse_datalog_bin --disable_inline -C $GIGAHORSE_DIR/clients/guards.dl_compiled,$GIGAHORSE_DIR/clients/visualizeout.py $HEX_FILE &> exec_info &&
+/usr/bin/time -v $GIGAHORSE_DIR/gigahorse.py -q QUIET -T $TIMEOUT --reuse_datalog_bin --disable_inline -C $GIGAHORSE_DIR/clients/guards.dl_compiled,$GIGAHORSE_DIR/clients/visualizeout.py $HEX_FILE &> exec_info &&
 curr_dir=$(pwd) && cd $GIGAHORSE_DIR && gigahorse_version=$(git rev-parse HEAD) && cd $curr_dir && printf "\tGigahorse version: $gigahorse_version\n" >> exec_info &&
 curr_dir=$(pwd) && cd $SETAAC_DIR && greed_version=$(git rev-parse HEAD) && cd $curr_dir && printf "\tgreed version: $greed_version\n" >> exec_info
 cp .temp/contract/out/* .
